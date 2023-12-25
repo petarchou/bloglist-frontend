a6ec22c2aadd2cf0bb475da4fc42d2cdbe0da8db
Magellan <ppetrov12@abv.bg>
2023-12-25 11:53:08 +0200

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,22 @@
 import Togglable from "./Togglable"
 
-const Blog = ({ blog }) => (
-  <div className="blog-container">
-    <span className="blogMainContent">{blog.title} {blog.author}</span>
-    <Togglable buttonDisplay='inline' buttonLabel='view'>banana</Togglable>
-  </div>  
-)
+const Blog = ({ blog }) => {
+
+  // console.log(`blog ${blog.title} has user ${blog.user}`)
+
+  return (
+    <div className="blog-container">
+      <span className="blogMainContent">{blog.title} by {blog.author}</span>
+      <Togglable buttonDisplay='inline' buttonLabel='view'>
+        <div>URL: {blog.url}</div>
+        <div>
+          <span>likes: {blog.likes}</span>
+          <button>like</button>
+          </div>
+        <div>User: {blog.user.name}</div>
+      </Togglable>
+    </div>
+  )
+} 
 
 export default Blog
